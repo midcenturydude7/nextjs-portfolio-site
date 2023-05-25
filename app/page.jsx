35630b8861c45e75af2ed00d50174353e5784f19
midCenturyDude7 <mgriffes@gmail.com>
2023-05-25 08:56:56 -0700

    Minor edits

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,11 @@ import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import CardSection from "./components/CardSection/CardSection";
 
-import styles from "./styles/page.module.css";
+import style from "./styles/page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
+    <div className={style.wrapper}>
       <Navbar />
       <HeroSection />
       <CardSection />
