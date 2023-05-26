0bfcfe6fd6afce7ed6a4385bc739fa6e6a6c1459
midCenturyDude7 <mgriffes@gmail.com>
2023-05-25 19:52:32 -0700

    Updated font-weight for 'Hi' title

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-  weight: "500",
+  weight: "600",
   subsets: ["latin"],
   weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
 });
