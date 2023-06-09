dfef5dfb4970659695e424fe505fee98ad640e9f
midCenturyDude7 <mgriffes@gmail.com>
2023-06-09 14:30:44 -0700

    Created data file for SkillsCard and SkillsGraph components

---
+++ data/cards.js
@@ -0,0 +1,98 @@
+export const cards = [
+  {
+    cardName: "Code",
+    cardItems: [
+      {
+        id: 1,
+        title: "HTML",
+        description: "HTML is the standard markup language for Web pages.",
+        text: "",
+      },
+      {
+        id: 2,
+        title: "CSS",
+        description: "CSS is the language we use to style an HTML document.",
+        text: "",
+      },
+      {
+        id: 3,
+        title: "JavaScript",
+        description:
+          "JavaScript is the programming language of HTML and the Web.",
+        text: "",
+      },
+      {
+        id: 4,
+        title: "TypeScript",
+        description:
+          "TypeScript is an open-source language which builds on JavaScript, one of the world’s most used tools, by adding static type definitions.",
+        text: "",
+      },
+    ],
+  },
+  {
+    cardName: "Tools",
+    cardItems: [
+      {
+        id: 1,
+        title: "React",
+        description:
+          "React is a JavaScript library for building user interfaces.",
+        text: "",
+      },
+      {
+        id: 2,
+        title: "Next.js",
+        description: "Next.js is a React framework for production.",
+        text: "",
+      },
+      {
+        id: 3,
+        title: "Node.js",
+        description:
+          "Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.",
+        text: "",
+      },
+      {
+        id: 4,
+        title: "SCSS",
+        description:
+          "SCSS is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets (CSS).",
+        text: "",
+      },
+    ],
+  },
+  {
+    cardName: "Skills",
+    cardItems: [
+      {
+        id: 1,
+        title: "UI/UX Design",
+        description:
+          "UI/UX design is the process of designing interfaces in software or computerized devices with a focus on looks or style.",
+        text: "",
+      },
+      {
+        id: 2,
+        title: "CSS Animations",
+        description:
+          "CSS animations make it possible to animate transitions from one CSS style configuration to another.",
+        text: "",
+      },
+      {
+        id: 3,
+        title: "Alogrithms & Data Structures",
+        description:
+          "Algorithms and data structures are the building blocks of programming.",
+        text: "",
+      },
+      {
+        id: 4,
+        title: "Git / GitHub",
+        description:
+          "Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency",
+        text: "",
+      },
+    ],
+  },
+];
