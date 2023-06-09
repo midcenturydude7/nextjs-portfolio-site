94cf7883210daef77425b38b937122ca9d18caf3
midCenturyDude7 <mgriffes@gmail.com>
2023-06-09 15:21:15 -0700

    Set up file/folder structure for SkillsGraph component

---
+++ app/components/SkillsGraph/SkillsGraph.jsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import SkillsCard from "../SkillsCard/SkillsCard";
+
+import style from "./styles/SkillsGraph.module.css";
+
+const SkillsGraph = () => {
+  return <div></div>;
+};
+
+export default SkillsGraph;
