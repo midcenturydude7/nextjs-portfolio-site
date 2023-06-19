de226e20c1cfaa6e83e96a6e47935e249a3035a7
midCenturyDude7 <mgriffes@gmail.com>
2023-06-19 09:47:26 -0700

    Updated content and styling of HTML box

--- app/components/SkillsCard/SkillsCardHtml.jsx
+++ app/components/SkillsCard/SkillsCardHtml.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { CgCloseO } from "react-icons/cg";
 import { FaLaptopCode, FaCodeBranch } from "react-icons/fa";
 
@@ -21,22 +22,70 @@ const SkillsCardHtml = ({ toggleCard }) => {
       <div className={style["skills-graph-content-container"]}>
         <div className={style["coursework-header"]}>
           <FaLaptopCode className={style["content-icon-html"]} />
-          <h3>Coursework</h3>
-        </div>
-        <div className={style["coursework-list"]}>
-          <ul className={style["coursework-list-items"]}>
-            <li>Scrimba Frontend Developer Certificate Program</li>
-            <li>Dave Gray&#39;s HTML Tutorial</li>
-          </ul>
-        </div>
-        <div className={style["coursework-header"]}>
-          <FaCodeBranch className={style["content-icon-html"]} />
           <h3>Projects</h3>
         </div>
         <div className={style["coursework-list"]}>
           <ul className={style["coursework-list-items"]}>
-            <li>Scrimba Frontend Developer Certificate Program</li>
-            <li>Dave Gray&#39;s HTML Tutorial</li>
+            <li className={style["main-list"]}>
+              <h4 className={style["course-title"]}>
+                <Link
+                  href="https://scrimba.com/learn/frontend"
+                  className={style["course-link"]}
+                  target="_blank">
+                  Scrimba Frontend Developer Certificate Program
+                </Link>
+              </h4>
+            </li>
+            <ul>
+              <li className={style["sub-list"]}>- Web dev basics</li>
+              <li className={style["sub-list"]}>
+                - Making websites interactive
+              </li>
+            </ul>
+            <li className={style["main-list"]}>
+              <h4 className={style["course-title"]}>
+                <Link
+                  href="https://www.youtube.com/playlist?list=PL0Zuz27SZ-6OlAwitnFUubtE93DO-l0vu"
+                  className={style["course-link"]}
+                  target="_blank">
+                  HTML Full Course for Beginners | Dave Gray
+                </Link>
+              </h4>
+            </li>
+          </ul>
+        </div>
+        <div className={style["coursework-header"]}>
+          <FaCodeBranch className={style["content-icon-html"]} />
+          <h3>Coursework</h3>
+        </div>
+        <div className={style["coursework-list"]}>
+          <ul className={style["coursework-list-items"]}>
+            <li className={style["main-list"]}>
+              <h4 className={style["course-title"]}>
+                <Link
+                  href="https://scrimba.com/learn/frontend"
+                  className={style["course-link"]}
+                  target="_blank">
+                  Scrimba Frontend Developer Certificate Program
+                </Link>
+              </h4>
+            </li>
+            <ul>
+              <li className={style["sub-list"]}>- Web dev basics</li>
+              <li className={style["sub-list"]}>
+                - Making websites interactive
+              </li>
+            </ul>
+            <li className={style["main-list"]}>
+              <h4 className={style["course-title"]}>
+                <Link
+                  href="https://www.youtube.com/playlist?list=PL0Zuz27SZ-6OlAwitnFUubtE93DO-l0vu"
+                  className={style["course-link"]}
+                  target="_blank">
+                  HTML Full Course for Beginners | Dave Gray
+                </Link>
+              </h4>
+            </li>
           </ul>
         </div>
       </div>
