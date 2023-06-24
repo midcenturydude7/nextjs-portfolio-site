4cdbd71cfa6cacabaff768f22ef1804e98da5b32
midCenturyDude7 <mgriffes@gmail.com>
2023-06-23 20:54:02 -0700

    Completed all content boxes for each skill

---
+++ app/components/SkillsCard/SkillsCardAnimations.jsx
@@ -0,0 +1,78 @@
+import SkillsCardItem from "./SkillsCardItem";
+import { CgCloseO } from "react-icons/cg";
+import { FaLaptopCode, FaCodeBranch } from "react-icons/fa";
+
+import style from "../ContentAbout/styles/ContentAbout.module.css";
+
+const SkillsCardAnimations = ({ toggleCard3 }) => {
+  return (
+    <div className={style["skills-graph"]}>
+      <ul>
+        <li className={style["header-width"]}>
+          <h2 className={`${style["skills-graph-title-left"]} ${style["small-text"]}`}>
+            CSS Animations
+          </h2>
+        </li>
+        <ul>
+          <li>
+            <button className={style["x-three"]} onClick={toggleCard3}>
+              <CgCloseO className={style["x-icon-three"]} />
+            </button>
+          </li>
+        </ul>
+      </ul>
+      <div className={style["skills-graph-content-container"]}>
+        <div className={style["coursework-header-three"]}>
+          <FaCodeBranch className={style["content-icon-three"]} />
+          <h3>Projects</h3>
+        </div>
+        <div className={style["coursework-list"]}>
+          <ul className={style["coursework-list-items"]}>
+            <SkillsCardItem
+              linkColor="course-link-three"
+              courseUrl="https://scrimba.com/learn/frontend"
+              courseText="Scrimba Frontend Developer Certificate Program"
+            />
+            <ul>
+              <li className={style["sub-list"]}>- Web dev basics</li>
+              <li className={style["sub-list"]}>
+                - Making websites interactive
+              </li>
+            </ul>
+            <SkillsCardItem
+              linkColor="course-link-three"
+              courseUrl="https://www.youtube.com/playlist?list=PL0Zuz27SZ-6OlAwitnFUubtE93DO-l0vu"
+              courseText="HTML Full Course for Beginners | Dave Gray"
+            />
+          </ul>
+        </div>
+        <div className={style["coursework-header-three"]}>
+          <FaLaptopCode className={style["content-icon-three"]} />
+          <h3>Coursework</h3>
+        </div>
+        <div className={style["coursework-list"]}>
+          <ul className={style["coursework-list-items"]}>
+            <SkillsCardItem
+              linkColor="course-link-three"
+              courseUrl="https://scrimba.com/learn/frontend"
+              courseText="Scrimba Frontend Developer Certificate Program"
+            />
+            <ul>
+              <li className={style["sub-list"]}>- Web dev basics</li>
+              <li className={style["sub-list"]}>
+                - Making websites interactive
+              </li>
+            </ul>
+            <SkillsCardItem
+              linkColor="course-link-three"
+              courseUrl="https://www.youtube.com/playlist?list=PL0Zuz27SZ-6OlAwitnFUubtE93DO-l0vu"
+              courseText="HTML Full Course for Beginners | Dave Gray"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SkillsCardAnimations;
--- app/components/SkillsGraph/SkillsGraphThree.jsx
+++ app/components/SkillsGraph/SkillsGraphThree.jsx
@@ -1,6 +1,9 @@
 import SkillsCard from "../SkillsCard/SkillsCard";
 import SkillsHeaderThree from "../SkillsHeader/SkillsHeaderThree";
 import SkillsCardDesign from "../SkillsCard/SkillsCardDesign";
+import SkillsCardAnimations from "../SkillsCard/SkillsCardAnimations";
+import SkillsCardAlgorithms from "../SkillsCard/SkillsCardAlgorithms";
+import SkillsCardGit from "../SkillsCard/SkillsCardGit";
 
 import cards from "../../../data/cards";
 import style from "../ContentAbout/styles/ContentAbout.module.css";
@@ -69,31 +72,31 @@ const SkillsGraphThree = ({
                 number={cards[2].cardItems[3].number}
                 skill={cards[2].cardItems[3].skill}
                 stroke={cards[2].cardItems[3].stroke}
-                onClickt={handleCard12Click}
+                onClick={handleCard12Click}
               />
             </div>
           )}
         </div>
       )}
-      {/* Skills: UX/UI DESIGN CONTENT */}
+      {/* SKILLS: UX/UI DESIGN CONTENT */}
       {isCard9Selected && isClicked3 && (
         <SkillsCardDesign toggleCard3={toggleCard3} />
       )}
 
-      {/* Skills: CSS CONTENT */}
-      {/* {isCard10Selected && isClicked3 && (
+      {/* SKILLS: CSS ANIMATIONS CONTENT */}
+      {isCard10Selected && isClicked3 && (
         <SkillsCardAnimations toggleCard3={toggleCard3} />
-      )} */}
+      )}
 
-      {/* Skills: JS CONTENT */}
-      {/* {isCard11Selected && isClicked3 && (
+      {/* SKILLS: ALGORITHMS & DATA STRUCTURES CONTENT */}
+      {isCard11Selected && isClicked3 && (
         <SkillsCardAlgorithms toggleCard3={toggleCard3} />
-      )} */}
+      )}
 
-      {/* Skills: TS CONTENT */}
-      {/* {isCard12Selected && isClicked3 && (
+      {/* SKILLS: GIT/GITHUB CONTENT */}
+      {isCard12Selected && isClicked3 && (
         <SkillsCardGit toggleCard3={toggleCard3} />
-      )} */}
+      )}
     </>
   );
 };
